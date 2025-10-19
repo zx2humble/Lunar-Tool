8bf82c63181699ac88d85658ad7265fd7652ce8a
ZX2 <128195646+zx2humble@users.noreply.github.com>
2025-10-19 11:35:08 -0400

    Update Lunar.lua

--- Lunar.lua
+++ Lunar.lua
@@ -794,7 +794,7 @@ function Library:create(options)
 
     local executorNameDisplay = profile:object("TextLabel", {
 		BackgroundTransparency = 1,
-		Position = UDim2.new(0, 105, 1, -20),
+		Position = UDim2.new(0, 105, 1, 40),
 		Size = UDim2.new(0, 400,0, 20),
 		AnchorPoint = Vector2.new(0, 1),
 		Theme = {TextColor3 = {"Tertiary", 10}},
