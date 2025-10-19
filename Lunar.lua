6813657c0c1b732453a0709d60039afc129e0849
ZX2 <128195646+zx2humble@users.noreply.github.com>
2025-10-19 07:43:21 -0400

    Update Lunar.lua

--- Lunar.lua
+++ Lunar.lua
@@ -829,7 +829,7 @@ function Library:create(options)
 	settingsTab:keybind{
 		Name = "Toggle Key",
 		Description = "Key to show/hide the UI.",
-		Keybind = Enum.KeyCode.insert,
+		Keybind = Enum.KeyCode.Insert,
 		Callback = function()
 			self.Toggled = not self.Toggled
 			Library:show(self.Toggled)
